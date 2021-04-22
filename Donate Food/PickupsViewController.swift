eb556d04bcfd719358fd193a9e6facc66a0d783a
jreese14 <jreesestige@gmail.com>
2021-04-21 22:17:01 -0700

    configured ui for tableview and created basic tableview functionality

---
+++ Donate Food/PickupsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PickupsViewController.swift
+//  Donate Food
+//
+//  Created by Jahnae Reese 2 on 4/21/21.
+//  Copyright © 2021 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class PickupsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = "row: \(indexPath.row)"
+        
+        return cell;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
