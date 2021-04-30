50d09cacbc5761d30203653a957d7f57a3e95c51
jreese14 <jreesestige@gmail.com>
2021-04-29 21:14:44 -0700

    Fixed restaurant home screen

---
+++ Donate Food/RestaurantProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantProfileViewController.swift
+//  Donate Food
+//
+//  Created by Jahnae Reese 2 on 4/29/21.
+//  Copyright © 2021 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
