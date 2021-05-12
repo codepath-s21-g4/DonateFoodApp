a29398a1ed3e5a2c54ee8435abd6b37a231d89fc
jreese14 <jreesestige@gmail.com>
2021-05-11 17:40:31 -0700

    updated API functions

--- Donate Food/API.swift
+++ Donate Food/API.swift
@@ -9,31 +9,28 @@
 import Foundation
 import UIKit
 struct API {
-    static func getPickups() {
-
-        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")
-        guard let requestUrl = url else { fatalError() }
-        // Create URL Request
-        var request = URLRequest(url: requestUrl)
-        // Specify HTTP Method to use
+    static func getPickups(completion: @escaping ([[String:Any]]?) -> Void) {
+       
+        
+        guard let url = URL(string: ProcessInfo.processInfo.environment["DATABASE_URL"]! + "/food_request/get-all") else { fatalError() }
+        var request = URLRequest(url: url)
+        
         request.httpMethod = "GET"
-        // Send HTTP Request
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-
-            // Check if Error took place
-            if let error = error {
-                print("Error took place \(error)")
-                return
-            }
-
-            // Read HTTP Response Status code
-            if let response = response as? HTTPURLResponse {
-                print("Response HTTP Status code: \(response.statusCode)")
-            }
-
-            // Convert HTTP Response Data to a simple String
+        
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
             if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
+                print("Response data string: \(dataString)")
+            }
+            
+            if let error = error {
+            print(error.localizedDescription)
+            } else if let data = data {
+            let dataDict = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+            let foodRequests = dataDict["food_requests"] as! [[String: Any]]
+            return completion(foodRequests)
             }
 
         }
@@ -75,4 +72,16 @@ struct API {
         task.resume()
         
     }
+    
+    static func postFoodRequest(pickupTime: String, foodType: String, quantity: String, dateCreated: String, points: Int) {
+        
+    
+    
+    
+    }
+    
+    
+    
 }
+
+
